0bf1c1069f2a4236db2b3d3365fdd34135fc3f95
V Chandrashekhar <v.chandrashekhar6561@gmail.com>
2024-07-04 11:19:17 +0530

    Time: 835 ms (83.29%) | Memory: 68.9 MB (58.79%) - LeetSync

---
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mergeNodes(self, head):
+        # Start with the first non-zero value.
+        head = head.next
+        if not head:
+            return head
+
+        # Initialize a dummy head node.
+        temp = head
+        sum = 0
+        while temp.val != 0:
+            sum += temp.val
+            temp = temp.next
+
+        # Store the sum in head's value.
+        head.val = sum
+        # Store head's next node as the recursive result for temp node.
+        head.next = self.mergeNodes(temp)
+        return head
