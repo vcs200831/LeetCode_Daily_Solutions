a3d07f471c416703ea3e4d7250328cb27c6516a3
V Chandrashekhar <v.chandrashekhar6561@gmail.com>
2025-01-18 10:56:01 +0530

    Time: 169 ms (13.70%) | Memory: 21.7 MB (88.36%) - LeetSync

---
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.py
@@ -0,0 +1,15 @@
+class Solution:
+    def doesValidArrayExist(self, derived: List[int]) -> bool:
+        # Create an original array initialized with 0.
+        original = [0]
+        for i in range(len(derived)):
+            original.append(derived[i] ^ original[i])
+
+        # Store the validation results in checkForZero and checkForOne respectively.
+        check_for_zero = original[0] == original[-1]
+        original = [1]
+        for i in range(len(derived)):
+            original.append(derived[i] ^ original[i])
+        check_for_one = original[0] == original[-1]
+
+        return check_for_zero or check_for_one
